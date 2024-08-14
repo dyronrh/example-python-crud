9749cf1628c5dc01efa3d2632b40af8c47518a7e
dyronrh <dyronrh@yahoo.es>
2024-08-14 17:19:08 -0400

    add new api version to test

--- tests/test_crud_api.py
+++ tests/test_crud_api.py
@@ -2,18 +2,18 @@ import time
 
 
 def test_root(test_client):
-    response = test_client.get("/v1/api//healthchecker")
+    response = test_client.get("/v1/api/healthchecker")
     assert response.status_code == 200
     assert response.json() == {"message": "The API is LIVE!!"}
 
 
 def test_create_get_user(test_client, user_payload):
-    response = test_client.post("/v1/api//users/", json=user_payload)
+    response = test_client.post("/v1/api/users/", json=user_payload)
     response_json = response.json()
     assert response.status_code == 201
 
     # Get the created user
-    response = test_client.get(f"/v1/api//users/{user_payload['id']}")
+    response = test_client.get(f"/v1/api/users/{user_payload['id']}")
     assert response.status_code == 200
     response_json = response.json()
     assert response_json["Status"] == "Success"
@@ -24,7 +24,7 @@ def test_create_get_user(test_client, user_payload):
 
 
 def test_create_update_user(test_client, user_payload, user_payload_updated):
-    response = test_client.post("/v1/api//users/", json=user_payload)
+    response = test_client.post("/v1/api/users/", json=user_payload)
     response_json = response.json()
     assert response.status_code == 201
 
@@ -33,7 +33,7 @@ def test_create_update_user(test_client, user_payload, user_payload_updated):
         1
     )  # Sleep for 1 second to ensure updatedAt is different (datetime precision is low in SQLite)
     response = test_client.patch(
-        f"/v1/api//users/{user_payload['id']}", json=user_payload_updated
+        f"/v1/api/users/{user_payload['id']}", json=user_payload_updated
     )
     response_json = response.json()
     assert response.status_code == 202
@@ -50,33 +50,33 @@ def test_create_update_user(test_client, user_payload, user_payload_updated):
 
 
 def test_create_delete_user(test_client, user_payload):
-    response = test_client.post("/v1/api//users/", json=user_payload)
+    response = test_client.post("/v1/api/users/", json=user_payload)
     response_json = response.json()
     assert response.status_code == 201
 
     # Delete the created user
-    response = test_client.delete(f"/v1/api//users/{user_payload['id']}")
+    response = test_client.delete(f"/v1/api/users/{user_payload['id']}")
     response_json = response.json()
     assert response.status_code == 202
     assert response_json["Status"] == "Success"
     assert response_json["Message"] == "User deleted successfully"
 
     # Get the deleted user
-    response = test_client.get(f"/v1/api//users/{user_payload['id']}")
+    response = test_client.get(f"/v1/api/users/{user_payload['id']}")
     assert response.status_code == 404
     response_json = response.json()
     assert response_json["detail"] == f"No User with this id: `{user_payload['id']}` found"
 
 
 def test_get_user_not_found(test_client, user_id):
-    response = test_client.get(f"/v1/api//users/{user_id}")
+    response = test_client.get(f"/v1/api/users/{user_id}")
     assert response.status_code == 404
     response_json = response.json()
     assert response_json["detail"] == f"No User with this id: `{user_id}` found"
 
 
 def test_create_user_wrong_payload(test_client):
-    response = test_client.post("/v1/api//users/", json={})
+    response = test_client.post("/v1/api/users/", json={})
     assert response.status_code == 422
 
 
@@ -84,7 +84,7 @@ def test_update_user_wrong_payload(test_client, user_id, user_payload_updated):
     user_payload_updated["first_name"] = (
         True  # first_name should be a string not a boolean
     )
-    response = test_client.patch(f"/v1/api//users/{user_id}", json=user_payload_updated)
+    response = test_client.patch(f"/v1/api/users/{user_id}", json=user_payload_updated)
     assert response.status_code == 422
     response_json = response.json()
     assert response_json == {
@@ -100,7 +100,7 @@ def test_update_user_wrong_payload(test_client, user_id, user_payload_updated):
 
 
 def test_update_user_doesnt_exist(test_client, user_id, user_payload_updated):
-    response = test_client.patch(f"/v1/api//users/{user_id}", json=user_payload_updated)
+    response = test_client.patch(f"/v1/api/users/{user_id}", json=user_payload_updated)
     assert response.status_code == 404
     response_json = response.json()
     assert response_json["detail"] == f"No User with this id: `{user_id}` found"
