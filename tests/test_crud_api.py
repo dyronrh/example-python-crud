ddb13d0733a2131eb71b3a2229ef5b0c2607d8cc
dyronrh <dyronrh@yahoo.es>
2024-08-14 23:15:10 -0400

    fix unit test

--- tests/test_crud_api.py
+++ tests/test_crud_api.py
@@ -2,7 +2,7 @@ import time
 
 
 def test_root(test_client):
-    response = test_client.get("/v1/api/healthchecker")
+    response = test_client.get("/api/healthchecker")
     assert response.status_code == 200
     assert response.json() == {"message": "The API is LIVE!!"}
 
